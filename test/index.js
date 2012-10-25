cb6f891eae0a4db09301863af0c79b326d0e62b4
Dominic Tarr <dominic.tarr@gmail.com>
2012-10-25 17:16:14 +0200

    test that can handle /

--- test/index.js
+++ test/index.js
@@ -14,14 +14,14 @@ rm('/tmp/kv_test', function () {
   var kv = require('..')('/tmp/kv_test/' + Math.random())
   var es = require('event-stream')
 
-  var p = kv.put('hello')
+  var p = kv.put('hello/there') //check that we can handle funny characters
   var r = Math.random()
 
   p.write({hello: r})
   p.end()
 
   p.on('close', function () {
-    var g = kv.get('hello')
+    var g = kv.get('hello/there')
     g.on('data', function (data) {
       assert.equal(data.hello, r)
       passed = true
@@ -30,7 +30,7 @@ rm('/tmp/kv_test', function () {
 
   kv.list()
     .pipe(es.writeArray(function (err, ary) {
-      assert.deepEqual(ary, ['hello'])
+      assert.deepEqual(ary, ['hello/there'])
     }))
 })
 
